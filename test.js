9f56d2f124de3359737a44170136b4925024ee05
ellmo <ellmunzai@gmail.com>
2013-12-31 18:13:11 +0100

    Exercise #3

--- test.js
+++ test.js
@@ -1,1 +1,29 @@
-/* JS goes here */
+$(document).ready(function(){
+  $('a[data-bind="sidebar_fn"]').on('click', onSidebarClick);
+  $('a[data-bind="blue_fn"]').on('mouseenter', feelBlue).on('mouseleave', blueNoMore)
+})
+
+function onSidebarClick(event){
+  window.setTimeout(toggleSidebar, 3000);
+}
+
+function toggleSidebar(event){
+  sidebar = $('#sidebar');
+  if(sidebar.data('showing')){
+    sidebar.animate({'width' : '0px'}, 100, function(){
+      sidebar.data('showing', false)
+    });
+  } else {
+    sidebar.animate({'width' : '300px'}, 100, function(){
+      sidebar.data('showing', true)
+    });
+  };
+}
+
+function feelBlue(event){
+  $('a:not([data-bind="blue_fn"])').css({'color': 'blue'});
+}
+
+function blueNoMore(event){
+  $('a:not([data-bind="blue_fn"])').css({'color': 'maroon'});
+}
